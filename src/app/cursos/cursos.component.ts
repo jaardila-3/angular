47fa8d370de022d07fa0cf6911cfbd7031abe863
jaardila-3 <jorge.ardila1641@correo.policia.gov.co>
2020-06-25 22:40:30 -0500

    creacion de un componente con angular-CLI, se debe cambiar el template y el styles en el objeto de cursos.component.ts

---
+++ src/app/cursos/cursos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cursos',
+  template: '<h1>Mis cursos</h1>',
+  styles: ['']
+})
+export class CursosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
