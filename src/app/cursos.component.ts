fec8141877f9dcd013b97dbe53a7cace15c00ad5
jaardila-3 <jorge.ardila1641@correo.policia.gov.co>
2020-06-25 19:55:52 -0500

    creacion primer componente en angular

---
+++ src/app/cursos.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'cursos',
+    template: `
+    <h1>Mis Cursos</h1>
+    <p>TypeScript</p>
+    `,
+    styles: ['h1 { color: red;}']
+})
+
+export class CursosComponent {
+
+}
